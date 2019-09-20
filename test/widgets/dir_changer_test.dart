9db914b78b3ff1c5bde98cc03fcd08be0c86ab1f
Hunter Breathat <breathat.hunter@gmail.com>
2019-09-20 10:15:26 -0400

    Added Base files for tests

---
+++ test/widgets/dir_changer_test.dart
@@ -0,0 +1,11 @@
+import 'package:filer_flutter_desktop/widgets/dir_changer.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Set up DirChanger', () {
+    DirChanger changer;
+    test('Default', () {
+      changer = DirChanger();
+    });
+  });
+}
